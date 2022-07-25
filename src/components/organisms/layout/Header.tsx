38493df4d3aab6e88dd00bdd28646f7e3226f891
hoshico <melancholinista.yasu@gmail.com>
2022-07-26 08:19:58 +0900

    add header

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,7 +1,12 @@
+import { Flex, Heading } from '@chakra-ui/react';
 import React, { FC, memo } from 'react'
 
 export const Header: FC = memo (() => {
   return (
-    <div style={{ height: "50px", backgroundColor: "teal" }}></div>
+    <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{ base: "3", md: "5"}}>
+      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+        ユーザー管理アプリ
+      </Heading>
+    </Flex>
   )
 });
