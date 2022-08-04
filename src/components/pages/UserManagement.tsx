da3e7cb4d671d6812dfb8efccebf772831308d29
hoshico <melancholinista.yasu@gmail.com>
2022-08-05 07:56:16 +0900

    add usercard

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,36 +1,13 @@
 import { Box, Image, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react'
 import React, { FC, memo } from 'react'
+import { UserCard } from '../organisms/user/UserCard'
 
 export const UserManagement: FC = memo(() => {
   return (
     <>
-      <Wrap>
+      <Wrap p={{ base: 4, md: 10 }}>
         <WrapItem>
-          <Box
-            w="260px"
-            h="260px"
-            bg="white"
-            borderRadius="10px"
-            shadow="md"
-            p={4}
-            _hover={{ cursor: "pointer", opacity: 0.8 }}
-          >
-            <Stack textAlign="center">
-              <Image
-                borderRadius="full"
-                boxSize="160px"
-                src="https://source.unsplash.com/random"
-                alt="プロフィール画像"
-                m="auto"
-              />
-              <Text fontSize="lg" fontWeight="bold">
-                やす
-              </Text>
-              <Text fontSize="sm" color="gray">
-                やす
-              </Text>
-            </Stack>
-          </Box>
+          <UserCard imageUrl="https://source.unsplash.com/random" userName="やす" fullName="ほしやす" />
         </WrapItem>
       </Wrap>
     </>
