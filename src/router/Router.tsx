225faeabfde28f930347505b823a47acec41f927
hoshico <melancholinista.yasu@gmail.com>
2022-08-15 03:49:35 +0900

    add loginuserprovider to router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,18 @@
-import { FC, memo } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Login } from "../components/pages/Login";
-import { Page404 } from "../components/pages/Page404";
-import { homeRoutes } from "./HomeRoutes";
+import { FC, memo } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Login } from '../components/pages/Login'
+import { LoginUserProvider } from '../providers/LoginUserProvider'
+import { homeRoutes } from './HomeRoutes'
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      {homeRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.children}/>    
-      ))} 
+      <LoginUserProvider>
+        <Route path="/" element={<Login />} />
+        {homeRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.children} />
+        ))}
+      </LoginUserProvider>
     </Routes>
   )
-});
+})
